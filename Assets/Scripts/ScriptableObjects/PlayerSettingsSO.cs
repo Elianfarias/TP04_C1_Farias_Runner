acad1c6d115a48f88c2053a95fc574a02a6936cb
Elian <dev.elian.farias@gmail.com>
2025-09-15 19:29:36 -0300

    add new slime violet with diferent speed

--- Assets/Scripts/ScriptableObjects/PlayerSettingsSO.cs
+++ Assets/Scripts/ScriptableObjects/PlayerSettingsSO.cs
@@ -5,6 +5,8 @@ public class PlayerSettingsSO : ScriptableObject
 {
     [SerializeField] private string playerName;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float volumeMusic;
+    [SerializeField] private float volumeSFX;
 
     public string PlayerName { get { return playerName; } }
 
@@ -18,5 +20,19 @@ public class PlayerSettingsSO : ScriptableObject
     public void SetJumpForce (float jumpForce)
     {
         this.jumpForce = jumpForce;
+    }
+
+    public float VolumeMusic { get { return volumeMusic; } }
+
+    public void SetVolumeMusic(float volumeMusic)
+    {
+        this.volumeMusic = volumeMusic;
+    }
+
+    public float VolumeSFX { get { return volumeSFX; } }
+
+    public void SetVolumeSFX(float volumeSFX)
+    {
+        this.volumeSFX = volumeSFX;
     }
 }
--- Assets/Scripts/UI/UIPlayerSettings.cs
+++ Assets/Scripts/UI/UIPlayerSettings.cs
@@ -24,6 +24,8 @@ public class UIPlayerSettings : MonoBehaviour
     [SerializeField] private Slider sliderSFX;
     [SerializeField] private TMP_Text txtSFXValue;
 
+    private const float DB_MIN = -80f;
+    private const float DB_MAX = 0f;
 
     private void Awake()
     {
@@ -36,6 +38,11 @@ public class UIPlayerSettings : MonoBehaviour
     private void Start()
     {
         inputName.text = playerSettings.PlayerName;
+        sliderMusic.value = playerSettings.VolumeMusic;
+        sliderSFX.value = playerSettings.VolumeSFX;
+
+        OnValueChangeMusic(sliderMusic.value);
+        OnValueChangeSFX(sliderSFX.value);
     }
 
     private void Update()
@@ -59,24 +66,25 @@ public class UIPlayerSettings : MonoBehaviour
     private void OnSaveClicked()
     {
         playerSettings.SetPlayerName(inputName.text);
+        playerSettings.SetVolumeMusic(sliderMusic.value);
+        playerSettings.SetVolumeSFX(sliderSFX.value);
 
         OnBackPause();
     }
 
     private void OnValueChangeMusic(float v)
     {
-        float normalized = (v / 80);
-        float percentage = normalized * 100f;
+        float vol = Mathf.InverseLerp(DB_MIN, DB_MAX, v) * 100f;
 
-        txtMusicValue.text = Mathf.Repeat(percentage, 80).ToString("0");
+        txtMusicValue.text = vol.ToString("0");
         audioMixer.SetFloat("VolumeMusic", v);
     }
 
     private void OnValueChangeSFX(float v)
     {
-        float normalized = (v - 80) / (0 - 80);
-        float percentage = normalized * 100f;
-        txtSFXValue.text = Mathf.Repeat(percentage, 80).ToString("0");
+        float vol = Mathf.InverseLerp(DB_MIN, DB_MAX, v) * 100f;
+
+        txtSFXValue.text = vol.ToString("0");
         audioMixer.SetFloat("VolumeSFX", v);
     }
 }
